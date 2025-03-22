94994fdb80a24f455bfe53e490b4e184eeffd218
unknown <student@COMP8.PROGRESH.LOCAL>
2025-03-22 09:25:50 +0300

    4

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button_like)
+        rollButton.setOnClickListener {
+            val rolltext_like: TextView = findViewById(R.id.text_like)
+            rolltext_like.text = "150000000"
+        }
+    }
+    }
